5a821ff6f198f48f1d3394304764a3a86d4deb52
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-26 20:19:02 -0500

    build

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue Framework Notesheet</h1>
+  <span> Follow along to learn more about how the Vue framework works.</span>
+  <TemplateSyntax msg="Template Syntax"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TemplateSyntax from './components/TemplateSyntax.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TemplateSyntax
   }
 }
 </script>
---
+++ src/components/TemplateSyntax.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="template-syntax">
+    <h2>{{ msg }}</h2>
+    <ol>
+      <li>{{ bindedText }}</li>
+      <li>Now we will perform a single Javascript expression within a binding --> <span>{{ showText ? 'Show this if showText is true' : 'this will display if showText is false' }}</span></li>
+      <li v-if="directive">Directives are attributes prefixed with <b>'v-'</b> and are single expressions intended to apply side affects to the DOM. This sentence as been rendered using <b>'v-if'</b></li>
+      <li>Directives can also take interchangable <span>Arguments</span> such as <b>'v-bind:href = 'url'</b>--> <a v-bind:href="url"> Check it out here!</a></li>
+      <li>Dynamic Arguments such as <b>'v-bind:[attributeName] = 'url'</b> or <b>'v-bind:[eventName]='doSomething'</b> are expected to evaluate a string. Non stringy values will trigger a warning. <button v-on:[click]="handleClick">Click Me</button></li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TemplateSyntax',
+  data () {
+    return {
+      bindedText: "This text was rendered using {{ }}",
+      showText: false,
+      directive: true,
+      url: 'https://vuejs.org/v2/guide/syntax.html',
+      click: 'click'
+
+    }
+  },
+  methods: {
+    handleClick () {
+      alert('This alert was triggered via a Dynamic Argument!')
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+li {
+  padding: 6px;
+}
+a {
+  color: #42b983;
+}
+span {
+  font-style: italic;
+}
+</style>
