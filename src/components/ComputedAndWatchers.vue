17efff58849eda0f6ed90f0de7bd01b3e1293b4e
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-27 15:55:26 -0500

    Computations in Vue

--- src/components/ComputedAndWatchers.vue
+++ src/components/ComputedAndWatchers.vue
@@ -4,6 +4,11 @@
         <ol>
             <li>For long expressions, use computed properties. This text has been reversed using a computed property <b>{{ computedSentence }}</b></li>
         </ol>
+        <p>So what is the difference between computed properties, watchers, and methods?</p>
+        <ol v-for="item in vueExpressions" :key="item.name">
+            <li>{{ item['name'] }} : {{ item['description']}}</li>
+        </ol>
+        <a :href="href">Learn More</a>
     </div>
 </template>
 
@@ -12,7 +17,22 @@ export default {
     name: 'ComputedAndWatchers',
     data () {
         return {
-            message: 'A sentence to perform computations on'
+            message: 'A sentence to perform computations on',
+            href:'https://flaviocopes.com/vue-methods-watchers-computed-properties/',
+            vueExpressions: [
+                {
+                name: 'Computed Property',
+                description: 'Reactive with caching based on reactive dependencies. Computed properties will only re-evaluate when reactive dependencies change.'
+                },
+                {
+                name: 'Methods',
+                description: 'to be filled in'
+                },
+                {
+                name: 'Watchers',
+                description: 'Used to perform asynchronous or expensive operations in response to changing data'
+                }
+            ]
 
         }
     },
@@ -26,3 +46,14 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ol {
+    list-style: none;
+}
+a {
+  color: #42b983;
+}
+
+    
+</style>
