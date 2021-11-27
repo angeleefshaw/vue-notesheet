264752ea2c23336d9b66b5e8cd6d5ba02c7b0774
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-27 09:37:22 -0500

    Update TemplateSyntax.vue
    Line breaks for readability

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -3,10 +3,20 @@
     <h2>{{ msg }}</h2>
     <ol>
       <li>{{ bindedText }}</li>
-      <li>Now we will perform a single Javascript expression within a binding --> <span>{{ showText ? 'Show this if showText is true' : 'this will display if showText is false' }}</span></li>
-      <li v-if="directive">Directives are attributes prefixed with <b>'v-'</b> and are single expressions intended to apply side affects to the DOM. This sentence as been rendered using <b>'v-if'</b></li>
-      <li>Directives can also take interchangable <span>Arguments</span> such as <b>'v-bind:href = 'url'</b>--> <a v-bind:href="url"> Check it out here!</a></li>
-      <li>Dynamic Arguments such as <b>'v-bind:[attributeName] = 'url'</b> or <b>'v-bind:[eventName]='doSomething'</b> are expected to evaluate a string. Non stringy values will trigger a warning. <button v-on:[click]="handleClick">Click Me</button></li>
+      <li>Now we will perform a single Javascript expression within a binding --> 
+        <span>{{ showText ? 'Show this if showText is true' : 'this will display if showText is false' }}</span>
+      </li>
+      <li v-if="directive">Directives are attributes prefixed with 
+        <b>'v-'</b> and are single expressions intended to apply side affects to the DOM. This sentence as been rendered using <b>'v-if'</b>
+      </li>
+      <li>Directives can also take interchangable 
+        <span>Arguments</span> such as <b>'v-bind:href = 'url'</b>--> 
+        <a v-bind:href="url"> Check it out here!</a>
+      </li>
+      <li>Dynamic Arguments such as <b>'v-bind:[attributeName] = 'url'
+        </b> or <b>'v-bind:[eventName]='doSomething'</b> are expected to evaluate a string. Non stringy values will trigger a warning. 
+        <button v-on:[click]="handleClick">Click Me</button>
+      </li>
     </ol>
   </div>
 </template>
