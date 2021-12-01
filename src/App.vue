b268ed30807e750129cb70addccf764445e4f3e1
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-11-30 19:12:58 -0500

    integrating store

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,30 @@
   <h1>Vue Framework Notesheet</h1>
   <span> Follow along to learn more about how the Vue framework works.</span>
   <TemplateSyntax class="m-5" msg="Template Syntax"/>
+  <button v-on:click="increment">Review Complete</button>
   <ComputedAndWatchers class="m-5" msg="Computed Properties and Watchers"/>
+  <button v-on:click="increment">Review Complete</button>
   </div>
 </template>
 
 <script>
 import TemplateSyntax from './components/TemplateSyntax.vue'
 import ComputedAndWatchers from './components/ComputedAndWatchers.vue'
+import count from './store/Count'
 
 export default {
   name: 'App',
   components: {
     TemplateSyntax,
     ComputedAndWatchers
+  },
+  store: count,
+  methods: {
+  increment() {
+    this.$store.commit('increment')
+    console.log(this.$store.state.count)
   }
+}
 }
 </script>
 
